2023ffb6f6829c0650c4944a92e1a184b0b6791a
Anthony Lepore <violacellonyc@gmail.com>
2020-08-11 22:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,15 @@ class Student < User
 attr_accessor :knowledge
 def initialize
   @knowledge = []
+end
 
+def learn(string)
+  @knowledge.push(string)
+end
+
+def knowledge 
+  @knowledge
+end
+
+  
 end
